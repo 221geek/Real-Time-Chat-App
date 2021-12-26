ffad0be5510167abad69b708f25191ba925c32ba
221geek <abdoulazizdione1997@gmail.com>
2021-12-26 16:24:33 +0000

    Prepend path prefix (/api) to all rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope(:path => '/api') do
+    resource :users, only: [:create]
+    post '/login', to: 'auth#login'
+  end
 end
