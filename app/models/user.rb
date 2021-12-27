d9e0748883756bd42a688e40b0ee0d018443b9e8
221geek <abdoulazizdione1997@gmail.com>
2021-12-27 14:57:07 +0000

    model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    before_create :prepare_username
+
+    has_secure_password
+
+    validates :username, :email, uniqueness: true
+    validates :first_name, :last_name, :email, presence: true
+    validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+
+    def prepare_username
+        self.username = (self.first_name + ' ' + self.last_name).to_s.downcase
+    end
 end
