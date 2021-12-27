a0e99dcd82abebcb12b05bcdf153f89161df2fa5
221geek <abdoulazizdione1997@gmail.com>
2021-12-27 14:56:20 +0000

    create migration

--- db/migrate/20211226141931_create_users.rb
+++ db/migrate/20211226141931_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :last_name
       t.string :username
       t.string :email
-      t.string :password
+      t.string :password_digest
       t.boolean :is_online, default: false
       t.datetime :last_seen
       t.timestamps
