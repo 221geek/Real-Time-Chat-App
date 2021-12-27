366949e6274541f040e1bf37c21382b2f5b551af
221geek <abdoulazizdione1997@gmail.com>
2021-12-27 14:16:15 +0000

    registration method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,23 @@
-class Api::UsersController < ApplicationController
+module Api
+    class UsersController < ApiController
+
+        def show
+            render json: true
+        end
+
+        def create
+            @new_user = User.new(user_params)
+
+            if @new_user.save
+                render json: @new_user, serializer: UserSerializer, status: :created
+            else
+                render json: @new_user.errors, status: :forbidden
+            end
+        end
+
+        private
+        def user_params
+            params.require(:user).permit(:first_name, :last_name, :email, :password)
+        end
+    end
 end
