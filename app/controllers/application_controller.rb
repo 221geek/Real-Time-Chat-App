1f2e782f7f11705b54a5e6ff4c6ebacfb44d202b
221geek <abdoulazizdione1997@gmail.com>
2021-12-27 14:15:29 +0000

    encode jwt token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+
+    def encode_token(payload, exp = 72.hours.from_now)
+        payload[:exp] = exp.to_i
+        JWT.encode(payload, Rails.application.secrets.secret_key_base)
+    end
+
 end
